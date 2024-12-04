5cd7a5d5aa85ab4ef0a83b6b00f70c95b2c2189a
Tb27-27 <2220046@talnet.nl>
2024-12-04 09:33:42 +0100

    Javascript fix
    Domloader moet voor alles apart worden gedaan, epic

--- js/MainJavaScript.js
+++ js/MainJavaScript.js
@@ -1,1 +1,16 @@
 
+
+// links in buttons naar andere paginas
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("contactKnop").addEventListener("click", function() {
+        window.location.href = "./Contact.html";
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("returnToMain").addEventListener("click", function() {
+        window.location.href = "./Main.html";
+    });
+});
