caa7eda89da9f1903b374aed38dc47c3e1561c70
Tb27-27 <2220046@talnet.nl>
2024-12-05 11:47:42 +0100

    javascript gefixt kinda
    waarschijnlijk niet heel scaleable, gaat er heel hardcodey uitzien
    
    low prio om te fixen, site werkt barely

--- js/MainJavaScript.js
+++ js/MainJavaScript.js
@@ -11,6 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("returnToMain").addEventListener("click", function() {
-        window.location.href = "./Main.html";
+        setTimeout(function() { document.getElementById("pageLeft").classList.add("closeBookAnimation"); }, 1); 
+        setTimeout(function() { document.getElementById("pageLeftBack").classList.add("closeBookAnimation"); }, 2);
+        setTimeout(function() { document.getElementById("bookCoverFrontInvertedFront").classList.add("closeBookAnimation"); }, 3); 
+        setTimeout(function() { document.getElementById("bookCoverFrontInvertedBack").classList.add("closeBookAnimation"); }, 4);
+        setTimeout(function() { document.getElementById("bookCoverFrontInvertedFront").style.zIndex = 15; }, 1000); 
+        setTimeout(function() { window.location.href = "./Main.html"; }, 2200);
     });
 });
+
