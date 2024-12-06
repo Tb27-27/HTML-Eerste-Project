74bdff17b3c866ac204eb9cbfe23dce28e47f6e5
Tb27-27 <2220046@talnet.nl>
2024-12-06 09:58:21 +0100

    javascript fix + zoom in and out
    main en contact werken nu ook met ids in plaats van classen

--- js/MainJavaScript.js
+++ js/MainJavaScript.js
@@ -2,21 +2,23 @@
 
 // links in buttons naar andere paginas
 document.addEventListener("DOMContentLoaded", function() {
-
     document.getElementById("contactKnop").addEventListener("click", function() {
-        window.location.href = "./Contact.html";
+        setTimeout(function() { document.body.classList.add('zoomIn'); }, 1);  
+        setTimeout(function() { document.getElementById("bookCover").classList.add("openBookAnimation"); }, 1); 
+        setTimeout(function() { document.getElementById("bookCoverBack").classList.add("openBookAnimation"); }, 2);
+        setTimeout(function() { document.getElementById("bookCoverBack").style.zIndex = 15; }, 1000);
+        setTimeout(function() { window.location.href = "./Contact.html"; }, 2200);
     });
-
 });
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("returnToMain").addEventListener("click", function() {
+        setTimeout(function() { document.body.classList.add('zoomOut'); }, 1);  
         setTimeout(function() { document.getElementById("pageLeft").classList.add("closeBookAnimation"); }, 1); 
         setTimeout(function() { document.getElementById("pageLeftBack").classList.add("closeBookAnimation"); }, 2);
         setTimeout(function() { document.getElementById("bookCoverFrontInvertedFront").classList.add("closeBookAnimation"); }, 3); 
         setTimeout(function() { document.getElementById("bookCoverFrontInvertedBack").classList.add("closeBookAnimation"); }, 4);
-        setTimeout(function() { document.getElementById("bookCoverFrontInvertedFront").style.zIndex = 15; }, 1000); 
+        setTimeout(function() { document.getElementById("bookCoverFrontInvertedFront").style.zIndex = 15; }, 1000);
         setTimeout(function() { window.location.href = "./Main.html"; }, 2200);
     });
 });
-
